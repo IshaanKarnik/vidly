1cf39c8993a3053f39fcec2ee3c686b07502daac
Ishaan Karnik <ishaan.k@tantrasoftsolutions.com>
2019-05-27 09:32:24 +0530

    Index force commit before pull

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const genres_router = require('./routes/genres');
+const mongoose      = require('mongoose');
 const express       = require('express');
 const helmet        = require('helmet');
 const morgan        = require('morgan');
@@ -11,3 +12,21 @@ app.use('/api/genres', genres_router);
 
 const port_no = process.env.PORT || 3000;
 app.listen(port_no, () => console.log(`Listening on port number ${port_no}`));
+
+async function connect() {
+    try{
+        await mongoose.connect(connection_string, { useNewUrlParser: true,
+                                                    dbName: database });
+        console.log("Collection : " + Course.collection.collectionName);
+        const result = await Course .find({ isPublished : true,
+                                            tags : 'backend' })
+                                    .sort({name : 1})
+                                    .select({name : 1, author : 1});
+        console.log(result);
+    }
+    catch(ex){
+        console.log(ex.message);
+    }
+}
+
+connect();
